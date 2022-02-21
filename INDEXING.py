bac90f57174b367b18029e895de3384fdfcdd245
Bhuvan1209 <99819094+Bhuvan1209@users.noreply.github.com>
2022-02-21 17:52:46 +0530

    Add files via upload

---
+++ INDEXING.py
@@ -0,0 +1,21 @@
+"""positive index list"""
+a=['besant','class']
+print(a[1])
+b=[1,2,3,4]
+print(b[2])
+"""negative index list"""
+a='this is my class room'
+print(a[-4])
+b=[1,2,3,4]
+print(b[-3])
+"""int,strng"""
+a=1,2,3,'run',4,'star'
+print(a[3][1])
+"""tuple"""
+a=(1,2,3,4)
+print(a[3])
+bhuva=('this is my name')
+print(bhuva[-3])
+bhuvanesh=('besant tech is good')
+print(bhuvanesh[0:5])
+
